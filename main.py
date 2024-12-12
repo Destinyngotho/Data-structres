52443714462020f815d491222e3b14ee7d045793
DESTINY WANJIRU <dwngotho@gmail.com>
2024-12-12 10:27:26 +0300

    data time

---
+++ main.py
@@ -0,0 +1,20 @@
+# list=[]
+# #empty list
+# list=[]
+# #list with integers
+# ls=[5,7,2,9]
+# print(ls)
+# print("No of items in list=",len(ls))
+# ls.append(25)
+# print("Add values in the list:",ls)
+# #remove the value
+# ls.remove(7)
+# print("list after remove 7:",ls)
+# #remove the last element
+# ls.pop()
+# print("list after remove last element:",ls)
+mydict={"studentname":"destiny","grade":7,"age":12}
+print(mydict)
+mydict["gender"]="female"
+print(mydict)
+print("Student age:",mydict["age"])
